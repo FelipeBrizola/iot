e75ad477064018d858eb0000e397280fd2f37f7d
Felipe Brizola <felipe_brizola@hotmail.com>
2018-01-25 17:14:12 -0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 USERNAME="deployer"
 HOST="104.41.29.178"
 SCRIPT="docker pull felipebrizola/alias; docker run -t -d -p 3000:3000 felipebrizola/alias /bin/bash"
+pwd
 ssh $USERNAME@$HOST "${SCRIPT}"
